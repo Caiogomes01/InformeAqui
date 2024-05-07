9df6dd7ac7437e67df7846e65654ae8b8d591840
Caiogomes01 <caiogomess013@gmail.com>
2024-05-06 21:49:03 -0300

    atualização 06/05

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categoria;
+use App\Models\Postagem;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $categoria_count = Categoria::count();
+        $postagem_count = Postagem::count();
+        $categorias = Categoria::get();
+        $postagem = Postagem::get();
+
+        return view('home',['categoria_count' => $categoria_count,
+                            'postagem_count'  => $postagem_count,
+                            'categorias'      => $categorias,
+                            'postagem'       => $postagem,
+
+                            ] );
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,33 @@
 @extends('adminlte::page')
 
 @section('content')
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work',     11],
+      ['Eat',      2],
+      ['Commute',  2],
+      ['Watch TV', 2],
+      ['Sleep',    7]
+    ]);
+
+    var options = {
+      title: 'Atividades'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+  }
+</script>
+
 <div class="container">
 
 <div class="row">
@@ -8,13 +35,13 @@
 
 <div class="small-box bg-info">
 <div class="inner">
-<h3>150</h3>
-<p>New Orders</p>
+<h3>{{ $categoria_count}}</h3>
+<p>Categoria</p>
 </div>
 <div class="icon">
 <i class="ion ion-bag"></i>
 </div>
-<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+<a href="{{ url('/categoria') }}" class="small-box-footer"> Mais informações <i class="fas fa-arrow-circle-right"></i></a>
 </div>
 </div>
 
@@ -22,45 +49,49 @@
 
 <div class="small-box bg-success">
 <div class="inner">
-<h3>53<sup style="font-size: 20px">%</sup></h3>
-<p>Bounce Rate</p>
+<h3>{{ $postagem_count }}<sup style="font-size: 20px"></sup></h3>
+<p>Postagem</p>
 </div>
 <div class="icon">
 <i class="ion ion-stats-bars"></i>
 </div>
-<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+<a href="{{ url('/postagem') }}" class="small-box-footer"> Mais informações <i class="fas fa-arrow-circle-right"></i></a>
 </div>
 </div>
-
 <div class="col-lg-3 col-6">
 
-<div class="small-box bg-warning">
-<div class="inner">
-<h3>44</h3>
-<p>User Registrations</p>
-</div>
-<div class="icon">
-<i class="ion ion-person-add"></i>
-</div>
-<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-</div>
+    <div class="small-box bg-warning">
+    <div class="inner">
+    <h3>44</h3>
+    <p>User Registrations</p>
+    </div>
+    <div class="icon">
+    <i class="ion ion-person-add"></i>
+    </div>
+    <a href="#" class="small-box-footer"> Mais informações  <i class="fas fa-arrow-circle-right"></i></a>
+    </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+
+    <div class="small-box bg-danger">
+    <div class="inner">
+    <h3>65</h3>
+    <p>Unique Visitors</p>
+    </div>
+    <div class="icon">
+    <i class="ion ion-pie-graph"></i>
+    </div>
+    <a href="#" class="small-box-footer"> Mais informações  <i class="fas fa-arrow-circle-right"></i></a>
+    </div>
+    </div>
+
+
+
 </div>
 
-<div class="col-lg-3 col-6">
+    <div id="piechart" style="width: 600px; height: 300px ;"></div>
 
-<div class="small-box bg-danger">
-<div class="inner">
-<h3>65</h3>
-<p>Unique Visitors</p>
-</div>
-<div class="icon">
-<i class="ion ion-pie-graph"></i>
-</div>
-<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-</div>
-</div>
-
-</div>
 
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -68,6 +99,7 @@
                 <div class="card-header">Olá Mundo!</div>
 
                 <div class="card-body">
+
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
